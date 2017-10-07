84f5938a0000e6a2957e992b97a86768fbcd29fb
Vadim Terekovsky <ota3300@mail.ru>
2017-10-07 13:48:36 +0300

    Install Fix

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := a3300
+PRODUCT_RELEASE_NAME := A3300
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -11,7 +11,7 @@ $(call inherit-product, device/Lenovo/A3300/device_A3300.mk)
 PRODUCT_LOCALES := ru_RU en_US uk_UA
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := A3300
+PRODUCT_DEVICE := a3300
 PRODUCT_NAME := lineage_A3300
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo IdeaTab A3300-HV
